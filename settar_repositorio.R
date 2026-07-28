2fcb3e92f4c1eccc692f23810d71a1e527677a79
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-28 10:44:52 -0300

    Criar repositóio e o README do repositório

---
+++ settar_repositorio.R
@@ -0,0 +1,23 @@
+# Pacotes ----
+
+library(usethis)
+
+library(gert)
+
+# Iniciar git ----
+
+usethis::use_git()
+
+# Commits ----
+
+gert::git_add(files = ".gitignore")
+
+gert::git_commit(message = "gitignore")
+
+# Criar repositório ----
+
+usethis::use_github()
+
+# Criar README ----
+
+usethis::use_readme_md()
