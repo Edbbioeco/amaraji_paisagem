0b2d09159b0be665382dc0b728affa33be11bddf
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-28 10:52:22 -0300

    Importar e visualizar o shapefile do Brasil

--- mapa_amaraji.R
+++ mapa_amaraji.R
@@ -11,3 +11,18 @@ library(tidyterra)
 library(cowplot)
 
 library(ggview)
+
+# Dados ----
+
+## Shapefile do Brasil ----
+
+### Importar ----
+
+br <- geobr::read_state(year = 2025)
+
+### Visualizar ----
+
+br
+
+ggplot() +
+  geom_sf(data = br,  color = "black")
