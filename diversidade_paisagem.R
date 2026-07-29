e15467c9954c3b8f5e76836ffe3643bef8b8ab4d
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-29 09:45:34 -0300

    Importar e visualizar o shapefile de Amaraji

--- diversidade_paisagem.R
+++ diversidade_paisagem.R
@@ -13,3 +13,17 @@ library(ggview)
 library(gganimate)
 
 library(landscapemetrics)
+
+# Shapefile de Amaraji----
+
+## Importar ----
+
+amaraji <- geobr::read_municipality(year = 2025) |>
+  dplyr::filter(name_muni == "Amaraji")
+
+## Visualizar ----
+
+amaraji
+
+ggplot() +
+  geom_sf(data = amaraji, color = "black")
