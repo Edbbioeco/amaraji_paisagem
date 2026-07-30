771510ceeb727a4101ceb000cb86ca66cd86e453
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-30 16:21:32 -0300

    Alterar as cores das categorias e a legenda da figura para dar os nomes das categorias

--- diversidade_paisagem.R
+++ diversidade_paisagem.R
@@ -101,16 +101,33 @@ purrr::imap(uso_solo_trat,
 
               ~ggplot() +
                 tidyterra::geom_spatraster(data = .x) +
-                scale_fill_viridis_d(option = "turbo",
-                                     na.translate = FALSE,
-                                     guide = guide_legend(
-
-                                       title.position = "top",
-                                       title.hjust = 0.5
-
-                                       )
-
-                                     ) +
+                scale_fill_manual(values = c("3" = "darkgreen",
+                                             "4" = "forestgreen",
+                                             "11" = "mediumseagreen",
+                                             "15" = "goldenrod",
+                                             "20" = "darkolivegreen3",
+                                             "21" = "darkolivegreen1",
+                                             "24" = "orangered4",
+                                             "33" = "blue"),
+                                  breaks = c("3",
+                                             "4",
+                                             "11",
+                                             "15",
+                                             "20",
+                                             "21",
+                                             "24",
+                                             "33"),
+                                  labels = c("Floresta",
+                                             "Savana",
+                                             "Campo alagado",
+                                             "Pastagem",
+                                             "Cana-de-açúcar",
+                                             "Mosaico de usos",
+                                             "Área urbana",
+                                             "Corpo hídrico"),
+                                  na.translate = FALSE) +
+                guides(fill = guide_legend(title.position = "top",
+                                            title.hjust = 0.5)) +
                 labs(title = .y) +
                 labs(title = paste0("Uso e cobertura do solo para ", .y),
                      subtitle = "Fonte: MapBiomas",
