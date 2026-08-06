d5c56ada65ab984cf2863426e1f0a75619b40cca
Edbbioeco <edsonbbiologia@gmail.com>
2026-08-05 21:38:52 -0300

    Baixar os rasters de uso e cobertura do solo

--- area_de_mata.R
+++ area_de_mata.R
@@ -30,3 +30,38 @@ amaraji
 
 ggplot() +
   geom_sf(data = amaraji, color = "black")
+
+# Raster de uso e cobertura do solo ----
+
+## Baixar ----
+
+uso_solo <- purrr::map(
+  1985:2025,
+  purrr::in_parallel(
+    \(periodo) {
+
+      tryCatch({
+
+        terra::rast(paste0(
+          "https://storage.googleapis.com/mapbiomas-public/initiatives/brasil/collection_10/lulc/coverage/brazil_coverage_",
+          periodo,
+          ".tif"
+        )) |>
+          terra::crop(amaraji) |>
+          terra::mask(amaraji)
+
+      }, error = \(e) {
+
+        message("Erro no ano ", periodo, ": ", e$message)
+        NULL
+
+      }
+
+      )
+
+    }
+
+  ), .progress = TRUE) |>
+  setNames(1985:2025 |> as.character())
+
+uso_solo
