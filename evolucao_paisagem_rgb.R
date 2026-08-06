87ea98db99ca1aeb62670e0e13fc5c24ce7ffdeb
Edbbioeco <edsonbbiologia@gmail.com>
2026-08-05 22:26:17 -0300

    Checar as cenas disponíveis entre 1985 a 2026

--- evolucao_paisagem_rgb.R
+++ evolucao_paisagem_rgb.R
@@ -40,3 +40,15 @@ ggplot() +
 cliente <- CDSE::GetOAuthClient(id = Sys.getenv("CDSE_ID"),
                                 secret = Sys.getenv("CDSE_SECRET"))
 
+### Carregar catálogo ----
+
+catalogo <- CDSE::SearchCatalog(aoi = amaraji,
+                                from = "1985-01-01",
+                                to = "2026-07-01",
+                                collection = "sentinel-2-l2a",
+                                with_geometry = FALSE,
+                                client = cliente,
+                                filter = "eo:cloud_cover < 5")
+
+catalogo
+
