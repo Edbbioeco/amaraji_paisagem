14a02f30ea32659a3eb7f9a303da33ac220eeb33
Edbbioeco <edsonbbiologia@gmail.com>
2026-08-05 22:14:11 -0300

    Importar o shapefile de Amaraji

--- temperatura_superficie.R
+++ temperatura_superficie.R
@@ -15,3 +15,14 @@ library(ggview)
 library(magic)
 
 library(gganimate)
+
+# Município de Amaraji ----
+
+## Importar ----
+
+amaraji <- geobr::read_municipality(
+  year = 2022,
+  code_muni = geobr::lookup_muni(name_muni = "Amaraji",
+                                 year = 2022) %>%
+    .$code_muni)
+
