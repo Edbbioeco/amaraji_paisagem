24e4809d6c8b356cb80f86f7ef0f32f6ebc4e428
Edbbioeco <edsonbbiologia@gmail.com>
2026-08-05 21:36:42 -0300

    Importar o shapefile de Amaraji

--- area_de_mata.R
+++ area_de_mata.R
@@ -11,3 +11,15 @@ library(tidyterra)
 library(ggview)
 
 library(magick)
+
+# Município de Amaraji ----
+
+## Importar ----
+
+## Importar ----
+
+amaraji <- geobr::read_municipality(
+  year = 2022,
+  code_muni = geobr::lookup_muni(name_muni = "Amaraji",
+                                 year = 2022) %>%
+    .$code_muni)
