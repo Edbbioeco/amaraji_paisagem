0a896462f267477f56bc63c071c051502efc9451
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-03 18:03:22 -0300

    Criar mapa de altitude

--- altitude_amaraji.R
+++ altitude_amaraji.R
@@ -44,3 +44,29 @@ alt
 ggplot() +
   tidyterra::geom_spatraster(data = alt) +
   scale_fill_viridis_c(na.value = "transparent")
+
+## Mapa ----
+
+ggplot() +
+  tidyterra::geom_spatraster(data = alt) +
+  tidyterra::scale_fill_hypso_c(
+    palette = "colombia_hypso",
+    na.value = "transparent",
+    direction = -1,
+    guide = guide_colourbar(title = "Atitude (m)",
+                            title.position = "top",
+                            title.hjust = 0.5,
+                            barheight = 2,
+                            barwidth = 30,
+                            frame.colour = "black",
+                            ticks.colour = "black")) +
+  theme_bw() +
+  theme(axis.text = element_text(color = "black", size = 20),
+        legend.text = element_text(color = "black", size = 20),
+        legend.title = element_text(color = "black", size = 20),
+        legend.position = "bottom",
+        panel.border = element_rect(color = "black", linewidth = 1)) +
+  ggview::canvas(height = 10, width = 12)
+
+ggsave(filename = "./amaraji_altitude.png",
+       height = 10, width = 12)
